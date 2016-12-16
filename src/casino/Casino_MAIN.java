9e2b42c80de50a379a85b414d1a8334478d3f500
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2016-12-16 17:10:10 -0600

    Commit.

--- src/casino/Casino_MAIN.java
+++ src/casino/Casino_MAIN.java
@@ -7,93 +7,83 @@ FINAL PROJECT
 package casino;
 import java.util.Scanner;
 
-public class Casino_MAIN 
-{ 
-    public static void main(String[] args) 
-    {        
-        
+public class Casino_MAIN {
+    public static void main(String[] args) {
+
         //"main" variables:
-        String strUserName;   
+        String strUserName;
         int intUserDeposit;
         char chrUserSelection;
         boolean bolDoLoopControl = true;
-        
+
         //create a Scanner object to read from the keyboard
-	Scanner keyboard = new Scanner(System.in);  
-        
+        Scanner keyboard = new Scanner(System.in);
+
         //Creates BANK object:
         Casino_BANK userAccount = new Casino_BANK();
-        
+
         //Gets user's name, and then greets the user:
-        System.out.println("Please enter your name: ");
-        strUserName = keyboard.next();        
-        System.out.println("Welcome to the casino, " + strUserName +". "
-                + "Please deposit between $20 to $1000 to begin: ");
-        
+        System.out.print("Please enter your name: ");
+        strUserName = keyboard.next();
+        System.out.print("Welcome to the casino, " + strUserName + ". " +
+            "Please deposit between $20 to $1000 to begin: ");
+
         //Gets user deposit amount, sees if it is enough or too much:
-        do
-        {
-           intUserDeposit = keyboard.nextInt();
-           if (intUserDeposit >= 20 && intUserDeposit <= 1000)
-           {
-               userAccount.AddToAccountBalance(intUserDeposit);
-               System.out.print("Your account has been created, starting at: $");
-               System.out.println(userAccount.GetAccountBalance());
-           }
-           else if (intUserDeposit < 20 || intUserDeposit > 1000)
-           {
-               System.out.println("TO REPEAT: Please deposit between $20 and $1000");
-           }
+        do {
+            intUserDeposit = keyboard.nextInt();
+            if (intUserDeposit >= 20 && intUserDeposit <= 1000) {
+                userAccount.AddToAccountBalance(intUserDeposit);
+                System.out.print("Your account has been created, starting at: $");
+                System.out.println(userAccount.GetAccountBalance());
+            } else if (intUserDeposit < 20 || intUserDeposit > 1000) {
+                System.out.print("TO REPEAT: Please deposit between $20 and $1000: ");
+            }
         }
         while (intUserDeposit < 20 || intUserDeposit > 1000);
-        
-        do
-        {
+
+        do {
             //If account below $20, ask player to add to it to bring it up above $20:
-            if (userAccount.GetAccountBalance() < 20)
-            {
-                do
-                {
+            if (userAccount.GetAccountBalance() < 20) {
+                do {
                     System.out.print("Your account is below $20, it is at: ");
                     System.out.println(userAccount.GetAccountBalance());
                     System.out.println("Please deposit enough funds to bring it above $20.");
                     intUserDeposit = keyboard.nextInt();
-                    userAccount.AddToAccountBalance(intUserDeposit);                    
-                }while (userAccount.GetAccountBalance() < 20);
+                    userAccount.AddToAccountBalance(intUserDeposit);
+                } while (userAccount.GetAccountBalance() < 20);
             }
-        //Game selection and user input:
-        System.out.println("Please choose your game: ");
-        System.out.println("PRESS '1' for BLACKJACK");
-        System.out.println("PRESS '2' for SLOT MACHINE");
-        System.out.println("PRESS 'Q' to QUIT");
-        
-        //accepts a char value:
-        chrUserSelection = keyboard.next().charAt(0);
-        
-        switch(chrUserSelection)
-                {
-                    case '1':
-                        Casino_BLACKJACK.BLACKJACK();
-                        System.out.println("Welcome back to the lobby.");
-                        break;
-                    
-                    case '2':
-                        Casino_SLOTMACHINE.SLOTMACHINE();
-                        System.out.println("Welcome back to the lobby.");
-                        break;
-                        
-                    case 'q':
-                    case 'Q':
-                        bolDoLoopControl = false;
-                        break;                       
-                    
-                }
-        }while (bolDoLoopControl == true);
-        
+            //Game selection and user input:
+            System.out.println("Please choose your game: ");
+            System.out.println("PRESS '1' for BLACKJACK");
+            System.out.println("PRESS '2' for SLOT MACHINE");
+            System.out.println("PRESS 'Q' to QUIT");
+
+            //accepts a char value:
+            chrUserSelection = keyboard.next().charAt(0);
+
+            switch (chrUserSelection) {
+                case '1':
+                    Casino_BLACKJACK.BLACKJACK();
+                    System.out.println("Welcome back to the lobby.");
+                    break;
+
+                case '2':
+                    Casino_SLOTMACHINE.SLOTMACHINE();
+                    System.out.println("Welcome back to the lobby.");
+                    break;
+
+                case 'q':
+                case 'Q':
+                    bolDoLoopControl = false;
+                    break;
+
+            }
+        } while (bolDoLoopControl == true);
+
         System.out.print("You end your tenure at the casino with : $");
         System.out.println(userAccount.GetAccountBalance());
-        
-        
+
+
     }
-    
+
 }
