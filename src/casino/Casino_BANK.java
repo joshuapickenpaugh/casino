015c6764aa04ecf54fb384864712b152b0c04f1f
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2016-12-16 17:10:00 -0600

    Commit.

--- src/casino/Casino_BANK.java
+++ src/casino/Casino_BANK.java
@@ -1,24 +1,20 @@
 package casino;
 
-public class Casino_BANK 
-{
+public class Casino_BANK {
     //Class variables:
     private static int intUserAccount;
-    
+
     //Accessor (Getter):
-    public static int GetAccountBalance()
-    {
+    public static int GetAccountBalance() {
         return intUserAccount;
     }
-    
+
     //Mutator (Setter); Adds winnings to user account:
-    public static void AddToAccountBalance(int usraccnt)
-    {
+    public static void AddToAccountBalance(int usraccnt) {
         intUserAccount += usraccnt;
     }
-    
-    public static void WithdrawlFromBalance(int wthdrwl)
-    {
+
+    public static void WithdrawlFromBalance(int wthdrwl) {
         intUserAccount = intUserAccount - wthdrwl;
     }
 }
